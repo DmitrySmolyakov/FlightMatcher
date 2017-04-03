5c5bc8f76e4756a42643d9ebe8e9fb07db93f574
Daria Korneichuk <1998dasha1998@inbox.ru>
2017-04-03 16:09:13 +0300

    Refactoring

--- FlightMatcher/FlightMatcher/Location.swift
+++ FlightMatcher/FlightMatcher/Location.swift
@@ -7,16 +7,54 @@
 //
 
 import UIKit
+import SwiftyJSON
 
 class Location {
     let id: Int
     let city: String
     let country: String
-    
-    init(with id: Int, city: String, country: String) {
+
+    init(id: Int, city: String, country: String) {
         self.id = id
         self.city = city
         self.country = country
     }
+    
+    convenience init?(json: JSON) {
+        guard let city = json["city"].string else { return nil}
+        guard let country = json["country"].string else { return nil}
+        guard let id = json["id"].int else { return nil}
+    
+        self.init(id: id, city: city, country: country)
+    }
+}
 
+extension Location {
+    public class func parse(file: String) -> [Location]? {
+        guard let file = Bundle.main.url(forResource: file, withExtension: "json") else {
+            return nil
+        }
+        do {
+            let data = try Data(contentsOf: file)
+            let json = JSON(data: data)
+            
+            var locations: [Location]? = [Location]()
+            var cityId = 0
+            
+            for (country,cities):(String, JSON) in json {
+                for (_,city):(String, JSON) in cities {
+                    guard let city = city.string else {
+                        return nil
+                    }
+                    let location = Location(id: cityId, city: city, country: country)
+                        locations?.append(location)
+                        cityId+=1
+                    
+                }
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+        return nil
+    }
 }
--- FlightMatcher/FlightMatcher/Request.swift
+++ FlightMatcher/FlightMatcher/Request.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SwiftyJSON
 
 class Request {
     let id: Int
@@ -15,12 +16,58 @@ class Request {
     let date: Date
     let flightNumber: Int
     
-    init(with id: Int, from: Location, to: Location, date: Date, flightNumber: Int) {
+    init(id: Int, from: Location, to: Location, date: Date, flightNumber: Int) {
         self.id = id
         self.from = from
         self.to = to
         self.date = date
         self.flightNumber = flightNumber
+    }
+}
+
+extension Request {
+    private class func getRequest(index: String, array: JSON) -> Request? {
+        let jsonFrom = array["from"]
+        guard let id = Int(index) else {
+            return nil
+        }
+        guard let fromLocation = Location(json: jsonFrom) else {
+            return nil
+        }
+        
+        let jsonTo = array["to"]
+        guard let toLocation = Location(json: jsonTo) else {
+            return nil
+        }
+        
+        guard let flightNumber = Int(array["flightNumber"].stringValue) else {
+            return nil
+        }
+        guard let unixNumber = Double(array["date"].stringValue) else {
+            return nil
+        }
+        let date = Date(timeIntervalSince1970: unixNumber)
+        
+        let request = Request(id: id, from: fromLocation, to: toLocation, date: date, flightNumber: flightNumber)
+        
+        return request
+    }
+    
+    public class func getRequests(url: URL) -> [Request]? {
+        do {
+            let data = try Data(contentsOf: url)
+            let json = JSON(data: data)
+            
+            var requests: [Request]? = [Request]()
+            
+            for (index, array):(String, JSON) in json {
+                requests?.append(getRequest(index: index, array: array)!)
+            }
+            return requests
+        } catch {
+            print(error.localizedDescription)
+        }
+        return nil
     }
     
 }
